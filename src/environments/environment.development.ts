52ecab1b636c9e3705bd5b4c52ae2c95242b2ce7
Joan Garcia <joangarcia2015@gmail.com>
2024-02-07 16:29:15 -0600

    Favicon and page title changed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    apiUrl: 'https://profit-app.onrender.com/profit-app/api'
-    // apiUrl: 'http://192.168.50.98:3000/profit-app/api'
+    apiUrl: 'http://192.168.50.98:3000/profit-app/api'
 };
